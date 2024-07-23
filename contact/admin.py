542343c6077f0f8b9a1e58eabbd555535ab4f0c3
Daniel Dias <danieldias.devjava@gmail.com>
2024-07-23 19:48:43 -0300

    criando o Model Category e fazendo a ligação com chave estrangeira

--- contact/admin.py
+++ contact/admin.py
@@ -12,3 +12,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 50
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
